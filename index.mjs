c4b37f3b116907fb9fa53f08af6048bfff9f5920
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-08-28 22:02:52 +0200

    evolux & thoregon: todo's, structure, loaders working

---
+++ index.mjs
@@ -0,0 +1,8 @@
+/**
+ * basic utils and es6 rebuilds to get the universe inflated
+ *
+ * @author: blukassen
+ */
+
+export { default as path }      from './lib/es6rebuilds/path.mjs';
+export *                        from './lib/utilfns.mjs';
