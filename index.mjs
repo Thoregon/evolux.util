7eb5840f27cc12682c3f34645a9862f2ec695b9e
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-01-06 17:46:56 +0100

    enqueue same requests

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@
 
 // export { default as path }       from './lib/es6rebuilds/path.mjs';
 export { debounce }                 from './lib/debounce.mjs';
+export { default as Q }             from './lib/requestqueue.mjs';
 export { default as parseIni }      from './lib/iniparser.mjs';
 export *                            from './lib/utilfns.mjs';
 export *                            from './lib/objutils.mjs';
