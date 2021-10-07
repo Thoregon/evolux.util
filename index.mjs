93c431e30154f8c4ac4461c9e8145fed432d96d1
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-10-07 15:21:50 +0200

    performance: introduce ResourceProxy (reduce request loops), thoregon.debug switch, utils, fixes, docu

--- index.mjs
+++ index.mjs
@@ -10,7 +10,7 @@ export { default as Q }             from './lib/requestqueue.mjs';
 export { default as parseIni }      from './lib/iniparser.mjs';
 export *                            from './lib/utilfns.mjs';
 export *                            from './lib/objutils.mjs';
-export { default as fmt }           from './lib/formatutils.mjs';
+export *                            from './lib/formatutils.mjs';
 export *                            from './lib/pathutils.mjs';
 export { default as ClassBuilder }  from './lib/classbuilder.mjs';
 export { default as RepoMirror }    from './lib/mixins/repomirror.mjs';
